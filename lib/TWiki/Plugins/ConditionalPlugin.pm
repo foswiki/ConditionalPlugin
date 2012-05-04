bd7d3f7a2a6d289e3b9f1f4f72cfb4d16efcb60b
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-05-04 13:27:49 +0000

    Item11808: perltidy them all, so people are not accidentally impacted by the new enforced rule -- Sorry Micha, could not find a way to define specific values within the file
    git-svn-id: http://svn.foswiki.org/trunk/ConditionalPlugin@14686 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/TWiki/Plugins/ConditionalPlugin.pm
+++ lib/TWiki/Plugins/ConditionalPlugin.pm
@@ -1,7 +1,7 @@
 # r1.2
 #
 # ConditionalPlugin for TWiki
-# Copyright (C) 2002 Jeroen van Dongen, jeroen@vthings.net 
+# Copyright (C) 2002 Jeroen van Dongen, jeroen@vthings.net
 #
 # TWiki WikiClone ($wikiversion has version info)
 # Copyright (C) 2000-2001 Andrea Sterbini, a.sterbini@flashnet.it
@@ -15,7 +15,7 @@
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details, published at 
+# GNU General Public License for more details, published at
 # http://www.gnu.org/copyleft/gpl.html
 #
 #
@@ -31,17 +31,16 @@
 # Change history:
 # r1.0 - initial revision
 # r1.1 - improved regexp to deal with multi-line spanning if/else constructs
-# r1.2 - minor improvements, added lazy-loading of Safe.pm 
+# r1.2 - minor improvements, added lazy-loading of Safe.pm
 #
-
 
 # =========================
 package TWiki::Plugins::ConditionalPlugin;
 
 # =========================
 use vars qw(
-         $VERSION $RELEASE $debug $sandbox $pluginInitialized
-    );
+  $VERSION $RELEASE $debug $sandbox $pluginInitialized
+);
 
 #use Safe;
 
@@ -55,36 +54,37 @@ $VERSION = '$Rev$';
 # of the version number in PLUGINDESCRIPTIONS.
 $RELEASE = 'Dakar';
 
-
-$prefixPattern  = '(^|[\s\-\*\(])';
-$ops      = '(<|>|<=|>=|lt|gt|le|ge|==|\!=|<=>|eq|ne|cmp|=~|\!~)';
-$condPattern_ifonly    = '(%IF{(\s*\w+\s*'.$ops.'\s*\w+\s*)}%(.*?)%ENDIF%)';
-$condPattern_ifelse    = '(%IF{(\s*\w+\s*'.$ops.'\s*\w+\s*)}%(.*?)%ELSE%(.*?)%ENDIF%)';
+$prefixPattern      = '(^|[\s\-\*\(])';
+$ops                = '(<|>|<=|>=|lt|gt|le|ge|==|\!=|<=>|eq|ne|cmp|=~|\!~)';
+$condPattern_ifonly = '(%IF{(\s*\w+\s*' . $ops . '\s*\w+\s*)}%(.*?)%ENDIF%)';
+$condPattern_ifelse =
+  '(%IF{(\s*\w+\s*' . $ops . '\s*\w+\s*)}%(.*?)%ELSE%(.*?)%ENDIF%)';
 $postfixPattern = '(?=[\s\.\,\;\:\!\?\)]*(\s|$))';
 
-
 # =========================
-sub initPlugin
-{
+sub initPlugin {
     ( $topic, $web, $user, $installWeb ) = @_;
 
     # check for Plugins.pm versions
-    if( $TWiki::Plugins::VERSION < 1 ) {
-        &TWiki::Func::writeWarning( "Version mismatch between ConditionalPlugin and Plugins.pm" );
+    if ( $TWiki::Plugins::VERSION < 1 ) {
+        &TWiki::Func::writeWarning(
+            "Version mismatch between ConditionalPlugin and Plugins.pm");
         return 0;
     }
 
     # Get plugin debug flag
-    $debug = &TWiki::Func::getPreferencesFlag( "CONDITIONALPLUGIN_DEBUG" );
+    $debug = &TWiki::Func::getPreferencesFlag("CONDITIONALPLUGIN_DEBUG");
 
-    # Don't initialize the sandbox yet. Loaded conditionally 
+    # Don't initialize the sandbox yet. Loaded conditionally
     # only when required.
     $pluginInitialized = 0;
+
     # create a sandbox to safely eval the condition expression
 
-
     # Plugin correctly initialized
-    &TWiki::Func::writeDebug( "- TWiki::Plugins::ConditionalPlugin::initPlugin( $web.$topic ) is OK" ) if $debug;
+    &TWiki::Func::writeDebug(
+        "- TWiki::Plugins::ConditionalPlugin::initPlugin( $web.$topic ) is OK")
+      if $debug;
     return 1;
 }
 
@@ -99,37 +99,42 @@ sub _initDefaults {
 }
 
 sub handleConditional {
-    my ($cond, $true_text, $false_text) = @_;
-    &TWiki::Func::writeDebug( "- Conditional: match $cond, $true_text, $false_text" ) if $debug;  
+    my ( $cond, $true_text, $false_text ) = @_;
+    &TWiki::Func::writeDebug(
+        "- Conditional: match $cond, $true_text, $false_text")
+      if $debug;
     return $sandbox->reval($cond) ? $true_text : $false_text;
 }
 
-
 # =========================
-sub commonTagsHandler
-{
+sub commonTagsHandler {
 ### my ( $text, $topic, $web ) = @_;   # do not uncomment, use $_[0], $_[1]... instead
 
-    &TWiki::Func::writeDebug( "- ConditionalPlugin::commonTagsHandler( $_[2].$_[1] )" ) if $debug;
+    &TWiki::Func::writeDebug(
+        "- ConditionalPlugin::commonTagsHandler( $_[2].$_[1] )")
+      if $debug;
 
     # check if there's an IF statement in this topic, otherwise we don't
     # have to load the lot.
-    if ( $_[0] !~ m/%IF{.*}%/) {
-	# nothing to do
-	&TWiki::Func::writeDebug( "- ConditionalPlugin: nothing to do" ) if $debug;	
-	return;
-    }
-    
-    _initDefaults() if( ! $pluginInitialized );
+    if ( $_[0] !~ m/%IF{.*}%/ ) {
 
-    # try to match if/else first (otherwise it gets masked by the if-only 
+        # nothing to do
+        &TWiki::Func::writeDebug("- ConditionalPlugin: nothing to do")
+          if $debug;
+        return;
+    }
+
+    _initDefaults() if ( !$pluginInitialized );
+
+    # try to match if/else first (otherwise it gets masked by the if-only
     # variant
-    $_[0] =~ s/$prefixPattern$condPattern_ifelse$postfixPattern/&handleConditional($3, $5, $6)/geo;
+    $_[0] =~
+s/$prefixPattern$condPattern_ifelse$postfixPattern/&handleConditional($3, $5, $6)/geo;
 
     # try to match the if-only variant
-    $_[0] =~ s/$prefixPattern$condPattern_ifonly$postfixPattern/&handleConditional($3, $5, '')/geos;
+    $_[0] =~
+s/$prefixPattern$condPattern_ifonly$postfixPattern/&handleConditional($3, $5, '')/geos;
 
 }
-
 
 1;
